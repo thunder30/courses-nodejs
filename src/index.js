903b0a602fb26b28711ba696b980139f800f8084
thunder30 <ngoctinh3004@gmail.com>
2021-08-27 22:30:32 +0700

    Static files and scss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+// config static file
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
