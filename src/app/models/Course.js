63551c7a1997c7768c8e43113c942137ea5678b6
thunder30 <ngoctinh3004@gmail.com>
2021-09-07 12:19:49 +0700

    Autoincrement field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
+// soft delete
 const mongooseDelete = require('mongoose-delete');
+// id tự động tăng
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Course = new Schema(
     {
+        _id: { type: Number },
         name: { type: String, required: true },
         description: { type: String },
         image: { type: String },
@@ -15,12 +19,14 @@ const Course = new Schema(
         level: { type: String },
     },
     {
+        _id: false,
         timestamps: true,
     },
 );
 
 // Add plugin
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
